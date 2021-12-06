705c9eee50b7ea7c8e5e92ea91f12a288721c91d
Igor Tyushev <igortimeweb@yandex.ru>
2021-12-06 22:42:02 +0300

    TheToaster - add UiToast component

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,24 +1,34 @@
 <template>
   <div class="toasts">
-    <div class="toast toast_success">
-      <ui-icon class="toast__icon" icon="check-circle" />
-      <span>Success Toast Example</span>
-    </div>
+    <ui-toast toast-type="success">Toast Example</ui-toast>
+    <ui-toast toast-type="success">Toast Example</ui-toast>
+    <ui-toast toast-type="error">Error Toast Example</ui-toast>
 
-    <div class="toast toast_error">
-      <ui-icon class="toast__icon" icon="alert-circle" />
-      <span>Error Toast Example</span>
-    </div>
+
+<!--    <div class="toast toast_error">-->
+<!--      <ui-icon class="toast__icon" icon="alert-circle" />-->
+<!--      <span>Error Toast Example</span>-->
+<!--    </div>-->
   </div>
 </template>
 
 <script>
 import UiIcon from './UiIcon';
+import UiToast from './UiToast';
 
 export default {
   name: 'TheToaster',
 
-  components: { UiIcon },
+  components: { UiIcon, UiToast },
+
+  methods: {
+    error: function (message) {
+      console.log(message);
+    },
+    success: function (message) {
+      console.log(message);
+    },
+  },
 };
 </script>
 
@@ -39,35 +49,5 @@ export default {
     bottom: 72px;
     right: 112px;
   }
-}
-
-.toast {
-  display: flex;
-  flex: 0 0 auto;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px;
-  background: #ffffff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
-  font-size: 18px;
-  line-height: 28px;
-  width: auto;
-}
-
-.toast + .toast {
-  margin-top: 20px;
-}
-
-.toast__icon {
-  margin-right: 12px;
-}
-
-.toast.toast_success {
-  color: var(--green);
-}
-
-.toast.toast_error {
-  color: var(--red);
 }
 </style>
